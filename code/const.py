70b8b78f43366e0370a4f037d352f9b4af2e0918
4nKsS <146961429+4nKsS@users.noreply.github.com>
2025-03-21 13:43:16 -0300

    Hit box Collisions

--- code/const.py
+++ code/const.py
@@ -29,33 +29,24 @@ ENTITY_HEALTH = {
     'background_5': 999,
     'background_6': 999,
     'background_7': 999,
-    'run_1': 300,
-    'go_1': 50,
+    'run_1': 100,
+    'go_1': 1,
 
 
 }
 
 ENTITY_DAMAGE = {
-    'Level1Bg0': 0,
-    'Level1Bg1': 0,
-    'Level1Bg2': 0,
-    'Level1Bg3': 0,
-    'Level1Bg4': 0,
-    'Level1Bg5': 0,
-    'Level1Bg6': 0,
-    'Level2Bg0': 0,
-    'Level2Bg1': 0,
-    'Level2Bg2': 0,
-    'Level2Bg3': 0,
-    'Level2Bg4': 0,
-    'Player1': 1,
-    'Player1Shot': 25,
-    'Player2': 1,
-    'Player2Shot': 20,
-    'Enemy1': 1,
-    'Enemy1Shot': 20,
-    'Enemy2': 1,
-    'Enemy2Shot': 15,
+    'background_0': 0,
+    'background_1': 0,
+    'background_2': 0,
+    'background_3': 0,
+    'background_4': 0,
+    'background_5': 0,
+    'background_6': 0,
+    'background_7': 0,
+    'run_1': 1,
+    'go_1': 10,
+
 }
 
 # M
